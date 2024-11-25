88855f10dc9da02d662e21aea77cd52726ef598a
aadarsh1503 <aadarshchauhan35@gmail.com>
2024-11-25 13:48:39 +0530

    done

--- client/src/components/About/CEOSpeaks.jsx
+++ client/src/components/About/CEOSpeaks.jsx
@@ -30,12 +30,12 @@ const CEOSpeaks = () => {
       {/* Content */}
       <p className="text-gray-700 mb-6">
         Nearly fifty years ago, AL SHAHEEN MANPOWER was founded with a dream. A dream to fulfill human
-        potential. It is a dream that we work on every day. Today, AL Shaheen Manpower opens doors for
+        potential. It is a dream that we work on every day. Today, Al Shaheen Manpower opens doors for
         international migrant workers and empowers them to earn a better livelihood. Recruiting across
         an array of industries including — Security, Facility Management, Construction, Food and Beverage,
         Retail, Hospitality, Aviation, Healthcare, and Oil and Gas, we have leveraged our vast experience
         servicing those sectors and created job-specific training courses to ensure that all workers deployed
-        by AL Shaheen Manpower arrive job-ready. AL Shaheen Manpower is approved by the Labour Market
+        by Al Shaheen Manpower arrive job-ready. Al Shaheen Manpower is approved by the Labour Market
         Regulatory Authority (<span
           className="font-bold text-lightblue cursor-pointer hover:underline"
           onClick={toggleModal}
@@ -47,11 +47,11 @@ const CEOSpeaks = () => {
       </p>
       <p className="text-gray-700 mb-6">
         Continually expanding our reach, we currently recruit from 32 countries across South Asia,
-        South-East Asia, Africa, Middle East, Eastern Europe, and Russia. AL SHAHEEN MANPOWER is committed
+        South-East Asia, Africa, Middle East, Eastern Europe, and Russia. Al Shaheen Manpower is committed
         to expanding its presence to newer employer markets, giving job-seekers a wide range of opportunities.
         This is complemented by our efforts to scale up the existing training infrastructure to cover all job
         roles, across our entire network. We are also optimizing our newly digitized processes to scale the
-        operational life cycle. As AL SHAHEEN MANPOWER enters the sixth decade of its life, the pursuit of our
+        operational life cycle. As Al Shaheen Manpower enters the sixth decade of its life, the pursuit of our
         dream is stronger than ever, as is our commitment to bringing skills and opportunities together.
       </p>
 
