7816834e82d9af08154a31824f6a5ed35f6ec875
aadarsh1503 <aadarshchauhan35@gmail.com>
2025-05-12 14:11:10 +0530

    done

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
-import { FaChevronDown, FaChevronUp, FaEnvelope ,FaBars, FaTimes } from 'react-icons/fa';
-import i1 from "./i1.jpg"
-import i21 from "./i21.jpg"
+import React, { useState, useRef } from 'react';
+import { FaChevronDown, FaChevronUp, FaEnvelope, FaBars, FaTimes } from 'react-icons/fa';
+import i1 from "./i1.jpg";
+import i21 from "./i21.jpg";
+
 const Navbar = () => {
   const [servicesDropdownOpen, setServicesDropdownOpen] = useState(false);
   const [industryDropdownOpen, setIndustryDropdownOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
   const [emailDropdownOpen, setEmailDropdownOpen] = useState(false);
-const toggleEmailDropdown = () => setEmailDropdownOpen(!emailDropdownOpen);
+  
+  const industryDropdownRef = useRef(null);  // Ref for the industry dropdown
+  const emailDropdownRef = useRef(null);     // Ref for the email dropdown
+
+  const toggleEmailDropdown = () => setEmailDropdownOpen(!emailDropdownOpen);
 
   const toggleServicesDropdown = () => {
     setServicesDropdownOpen(!servicesDropdownOpen);
@@ -35,7 +40,6 @@ const toggleEmailDropdown = () => setEmailDropdownOpen(!emailDropdownOpen);
 
   return (
     <nav className="relative bg-white lg:flex max-w-7xl mx-auto justify-end items-center p-6 lg:py-4 text-black">
-      {/* Logo positioned to the left and partially outside navbar */}
       <div className="absolute left-1 top-1 z-20 hidden lg:block">
         <img
           src={i21}
@@ -44,7 +48,6 @@ const toggleEmailDropdown = () => setEmailDropdownOpen(!emailDropdownOpen);
         />
       </div>
 
-      {/* Hamburger icon for mobile */}
       <button
         className="lg:hidden text-DarkRed text-2xl z-50 relative"
         onClick={toggleMenu}
@@ -52,203 +55,100 @@ const toggleEmailDropdown = () => setEmailDropdownOpen(!emailDropdownOpen);
         {menuOpen ? <FaTimes /> : <FaBars />}
       </button>
 
-      {/* Navbar Links for Desktop */}
       <div className="hidden lg:text-md lg:flex space-x-8">
         <a href="/" className="hover:underline text-DarkRed lg:mt-2 mt-0 font-semibold hover:text-lightgreen">Home</a>
         <a href="/about" className="hover:underline font-semibold lg:mt-2 mt-0 hover:text-lightgreen text-DarkRed">About Us</a>
         
-        {/* Services Dropdown */}
-        {/* <div className="relative">
-          <button
-            className="hover:underline font-semibold hover:text-lightgreen text-DarkRed flex items-center"
-            onClick={toggleServicesDropdown}
-          >
-            Services
-            {servicesDropdownOpen ? <FaChevronUp className="ml-2 mt-1" /> : <FaChevronDown className="ml-2 mt-1" />}
-          </button>
-          {servicesDropdownOpen && (
-            <div className="absolute z-50 font-raleway mt-2 bg-white shadow-lg w-48 rounded-md">
-              <button
-                className="block px-4 py-2 text-black hover:text-lightgreen hover:bg-gray-100"
-                onClick={() => handleSelection('/recruitment')}
-              >
-                Recruitment
-              </button>
-              <button
-                className="block px-4 py-2 text-black hover:text-lightgreen hover:bg-gray-100"
-                onClick={() => handleSelection('/pre-employment')}
-              >
-                Pre-Employment
-              </button>
-              <button
-                className="block px-4 py-2 text-black hover:text-lightgreen hover:bg-gray-100"
-                onClick={() => handleSelection('/placements')}
-              >
-                Placements
-              </button>
-              <button
-                className="block px-4 py-2 text-black hover:text-lightgreen hover:bg-gray-100"
-                onClick={() => handleSelection('/visas')}
-              >
-                Visas
-              </button>
-            </div>
-          )}
-        </div> */}
-
-        {/* Industry Dropdown */}
-        <div className="relative">
-          <button
+        <div className="relative" ref={industryDropdownRef}>
+          <div
             className="hover:underline font-semibold hover:text-lightgreen lg:mt-2 mt-0 text-DarkRed flex items-center"
-            onClick={toggleIndustryDropdown}
+            onMouseEnter={() => {
+              setIndustryDropdownOpen(true);
+              setServicesDropdownOpen(false);
+            }}
+            onMouseLeave={(e) => {
+              // Close dropdown only if the mouse leaves both the parent and the dropdown
+              if (!industryDropdownRef.current.contains(e.relatedTarget)) {
+                setIndustryDropdownOpen(false);
+              }
+            }}
           >
             Industries
             {industryDropdownOpen ? <FaChevronUp className="ml-2 mt-1" /> : <FaChevronDown className="ml-2 mt-1" />}
-          </button>
+          </div>
           {industryDropdownOpen && (
-  <div className="absolute z-50 font-raleway mt-2 bg-white shadow-lg w-48 rounded-md">
-    <button
-      className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100"
-      onClick={() => handleSelection('/aviation')}
-    >
-      Aviation
-    </button>
-              <button
-                className="block w-full text-left px-4 py-2 text-black hover:text-lightgreen hover:bg-gray-100"
-                onClick={() => handleSelection('/construction')}
-              >
-                Construction
-              </button>
-              <button
-                className="block px-4 py-2 w-full text-left text-black hover:text-lightgreen hover:bg-gray-100"
-                onClick={() => handleSelection('/events')}
-              >
-                Events
-              </button>
-              <button
-                className="block px-4 py-2 w-full text-left text-black hover:text-lightgreen hover:bg-gray-100"
-                onClick={() => handleSelection('/healthcare')}
-              >
-                Healthcare
-              </button>
-              <button
-                className="block px-4 py-2 w-full text-left text-black hover:text-lightgreen hover:bg-gray-100"
-                onClick={() => handleSelection('/hospitality')}
-              >
-                Hospitality
-              </button>
-              <button
-                className="block px-4 py-2 w-full text-left text-black hover:text-lightgreen hover:bg-gray-100"
-                onClick={() => handleSelection('/logistics')}
-              >
-                Logistics
-              </button>
-              <button
-                className="block px-4 py-2 w-full text-left text-black hover:text-lightgreen hover:bg-gray-100"
-                onClick={() => handleSelection('/manufacturing')}
-              >
-                Manufacturing
-              </button>
-              <button
-                className="block px-4 py-2 w-full text-left text-black hover:text-lightgreen hover:bg-gray-100"
-                onClick={() => handleSelection('/retail')}
-              >
-                Retail
-              </button>
-              <button
-                className="block px-4 py-2 w-full text-left text-black hover:text-lightgreen hover:bg-gray-100"
-                onClick={() => handleSelection('/security')}
-              >
-                Security
-              </button>
+            <div
+              className="absolute z-50 font-raleway mt-2 bg-white shadow-lg w-48 rounded-md"
+              onMouseEnter={() => setIndustryDropdownOpen(true)}
+              onMouseLeave={() => setIndustryDropdownOpen(false)}
+            >
+              {/* Dropdown links */}
+              <a href="/aviation" className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100">Aviation</a>
+              <a href="/construction" className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100">Construction</a>
+              <a href="/events" className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100">Events</a>
+              <a href="/healthcare" className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100">Healthcare</a>
+              <a href="/hospitality" className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100">Hospitality</a>
+              <a href="/logistics" className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100">Logistics</a>
+              <a href="/manufacturing" className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100">Manufacturing</a>
+              <a href="/retail" className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100">Retail</a>
+              <a href="/security" className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100">Security</a>
             </div>
           )}
         </div>
 
         <a href="/training" className="hover:underline font-semibold hover:text-lightgreen lg:mt-2 mt-0 text-DarkRed">Training</a>
-
         <a href="/newsEvents" className="hover:underline font-semibold hover:text-lightgreen lg:mt-2 mt-0 text-DarkRed">News</a>
         <a href="/contactUs" className="hover:underline text-DarkRed font-semibold lg:mt-2 mt-0 hover:text-lightgreen">Contact Us</a>
-        <div className="relative">
-  <button
-    className="hover:underline font-semibold hover:text-lightgreen lg:mt-2 mt-0 text-DarkRed flex items-center"
-    onClick={toggleEmailDropdown}
-  >
-    <FaEnvelope className="mr-2" /> Email
-    {emailDropdownOpen ? <FaChevronUp className="ml-2 mt-1" /> : <FaChevronDown className="ml-2 mt-1" />}
-  </button>
-  {emailDropdownOpen && (
-    <div className="absolute z-50 font-raleway mt-2 bg-white shadow-lg w-48 rounded-md">
-      {/* Customer Care */}
-    
+        
+        <div className="relative" ref={emailDropdownRef}>
+          <div
+            className="hover:underline font-semibold hover:text-lightgreen lg:mt-2 mt-0 text-DarkRed flex items-center"
+            onMouseEnter={() => {
+              setEmailDropdownOpen(true);
+            }}
+            onMouseLeave={(e) => {
+              // Close dropdown only if the mouse leaves both the parent and the dropdown
+              if (!emailDropdownRef.current.contains(e.relatedTarget)) {
+                setEmailDropdownOpen(false);
+              }
+            }}
+          >
+            <FaEnvelope className="mr-2" /> Email
+            {emailDropdownOpen ? <FaChevronUp className="ml-2 mt-1" /> : <FaChevronDown className="ml-2 mt-1" />}
+          </div>
+          {emailDropdownOpen && (
+            <div 
+              className="absolute z-50 font-raleway mt-2 bg-white shadow-lg w-48 rounded-md"
+              onMouseEnter={() => setEmailDropdownOpen(true)}
+              onMouseLeave={() => setEmailDropdownOpen(false)}
+            >
+              <a href="mailto:sales@alshaheen.pro" className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100">
+                Seller Care
+              </a>
+              <a href="mailto:info@alshaheen.pro" className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100">
+                Business Enquiries
+              </a>
+            </div>
+          )}
+        </div>
 
-      {/* Sales */}
-      <a 
-        href="mailto:sales@alshaheen.pro" 
-        className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100"
-        title="Send an email to Sales"
-      >
-        Seller Care
-      </a>
-
-      {/* Business Enquiries */}
-      <a 
-        href="mailto:info@alshaheen.pro" 
-        className="block w-full px-4 py-2 text-left text-black hover:text-lightgreen hover:bg-gray-100"
-        title="Send an email to Business Enquiries"
-      >
-        Business Enquiries
-      </a>
-    </div>
-  )}
-</div>
-
-
-
-<a  
-  href="https://www.talentportal.bh/#pills-home" 
-  target="_blank"  
-  className="bg-red-600 text-white lg:text-md  font-semibold hover:text-DarkRed outline hover:outline-Darkred hover:bg-white mt-0 px-4 py-2 rounded"
->
-  Click Here To Register
-</a>
-
+        <a href="https://www.talentportal.bh/#pills-home" target="_blank" className="bg-red-600 text-white lg:text-md  font-semibold hover:text-DarkRed outline hover:outline-Darkred hover:bg-white mt-0 px-4 py-2 rounded">
+          Click Here To Register
+        </a>
       </div>
 
       {/* Mobile Menu */}
-      <div
-  className={`lg:hidden fixed bottom-0 left-0 w-full h-full bg-white shadow-lg transform ${menuOpen ? 'translate-y-0' : '-translate-y-full'} transition-transform duration-300 ease-in-out z-40`}
->
-  <ul className="flex flex-col  items-center justify-center h-full space-y-4 p-4">
-    <img
-      src="https://alshaheen.pro/wp-content/uploads/2023/09/cropped-AlshaheenManpower_1_page-0001.jpg"
-      alt="Logo"
-      className="h-28"
-    />
-    <li><a href="/" className="text-DarkRed font-semibold hover:text-lightgreen text-2xl">Home</a></li>
-    <li><a href="/about" className="font-semibold hover:text-lightgreen text-DarkRed text-2xl">About Us</a></li>
+      <div className={`lg:hidden fixed bottom-0 left-0 w-full h-full bg-white shadow-lg transform ${menuOpen ? 'translate-y-0' : '-translate-y-full'} transition-transform duration-300 ease-in-out z-40`}>
+        <ul className="flex flex-col items-center justify-center h-full space-y-4 p-4">
+          <img
+            src={i21}
+            alt="Logo"
+            className="h-28"
+          />
+          {/* Mobile Menu Items */}
+          <li className="relative space-y-4 w-full text-center">
 
-    {/* Mobile Services Dropdown */}
-    {/* <li className="relative w-full text-center">
-      <button
-        className="font-semibold hover:text-lightgreen text-DarkRed flex items-center justify-center w-full text-2xl"
-        onClick={toggleServicesDropdown}
-      >
-        Services
-        {servicesDropdownOpen ? <FaChevronUp className="ml-2" /> : <FaChevronDown className="ml-2" />}
-      </button>
-      {servicesDropdownOpen && (
-        <ul className="mt-2 space-y-2 text-lg">
-          <li><a href="/recruitment" className="block text-black hover:text-lightgreen">Recruitment</a></li>
-          <li><a href="/pre-employment" className="block text-black hover:text-lightgreen">Pre-Employment</a></li>
-          <li><a href="/placements" className="block text-black hover:text-lightgreen">Placements</a></li>
-          <li><a href="/visas" className="block text-black hover:text-lightgreen">Visas</a></li>
-        </ul>
-      )}
-    </li> */}
-
-    {/* Mobile Industry Dropdown */}
-    <li className="relative w-full text-center">
+          <a href="/about" className="font-semibold hover:text-lightgreen text-DarkRed flex items-center justify-center w-full text-2xl">About Us</a>
       <button
         className="font-semibold hover:text-lightgreen text-DarkRed flex items-center justify-center w-full text-2xl"
         onClick={toggleIndustryDropdown}
@@ -287,7 +187,6 @@ const toggleEmailDropdown = () => setEmailDropdownOpen(!emailDropdownOpen);
       </button>
       {emailDropdownOpen && (
         <div className="mt-2 grid grid-cols-1 gap-4 w-full px-4 py-4 bg-white border rounded-lg shadow-md">
-         
           <a 
             href="mailto:sales@alshaheen.pro" 
             className="block text-center py-2 px-4 text-black hover:text-lightgreen hover:bg-gray-100 rounded-md"
@@ -303,9 +202,8 @@ const toggleEmailDropdown = () => setEmailDropdownOpen(!emailDropdownOpen);
         </div>
       )}
     </div>
-  </ul>
-</div>
-
+        </ul>
+      </div>
     </nav>
   );
 };
