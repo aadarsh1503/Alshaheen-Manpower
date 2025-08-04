ab6baad96650444740ef2d5c8c597cb0258053f8
aadarsh1503 <aadarshchauhan35@gmail.com>
2025-08-04 13:55:49 +0530

    done

--- server/index.js
+++ server/index.js
@@ -14,8 +14,28 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// --- CORS Configuration ---
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://gvs-services.vercel.app',
+  'https://alshaheen.pro'
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,POST,PUT,DELETE,PATCH,HEAD,OPTIONS',
+  credentials: true
+};
+
 // --- Global Middleware ---
-app.use(cors());
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -41,8 +61,13 @@ app.get('/', (req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Internal Server Error');
+  if (err.message === 'Not allowed by CORS') {
+    console.error('CORS Blocked Request from:', req.headers.origin);
+    res.status(403).json({ error: 'This origin is not authorized to access this resource.' });
+  } else {
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // --- Start Server ---
